d55e9f3f03278ac25f7b62f96713c9c0a638543a
pdefaz <pldefaz@espe.edu.ec>
2026-07-30 20:55:12 -0500

    chore: inicializa proyecto agrosmart con dependencias base

---
+++ src/main/java/ec/edu/espe/agrosmart/AgrosmartApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.agrosmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrosmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrosmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/agrosmart/AgrosmartApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.agrosmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrosmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
